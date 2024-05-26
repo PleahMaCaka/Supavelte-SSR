5bc8b4836d3245b891c9e6e28ffa7dbb6db2715d
PleahMaCaka <pleahmacaka@gmail.com>
2024-05-27 01:54:40 +0900

    chore(deps): init supabase ssr

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,7 +1,18 @@
+// noinspection JSUnusedGlobalSymbols
+
+import type { Session, SupabaseClient, User } from "@supabase/supabase-js"
+
 declare global {
     namespace App {
         // interface Error {}
-        // interface Locals {}
+        
+        interface Locals {
+            supabase: SupabaseClient
+            safeGetSession: () => Promise<{ session: Session | null; user: User | null }>
+            session: Session | null
+            user: User | null
+        }
+
         // interface PageData {}
         // interface PageState {}
         // interface Platform {}
