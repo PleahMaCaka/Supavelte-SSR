4416aa76be5b72631e55f67115fdca0cbd5dab67
PleahMaCaka <pleahmacaka@gmail.com>
2024-05-27 00:32:04 +0900

    chore(deps): added TailwindCSS and DaisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl font-bold underline text-center">
+	Hello world!
+</h1>
+<button class="btn btn-primary size-full h-full
+							text-white text-3xl"
+>
+	BIG HELLO
+</button>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// noinspection JSUnusedGlobalSymbols
+/** @type {import("tailwindcss").Config} */
+export default {
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+	theme: {
+		extend: {}
+	},
+	plugins: [
+		require("daisyui")
+	]
+}
