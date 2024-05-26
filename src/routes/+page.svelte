51be864a806bd6825f8c9813265268dffd31ecb0
PleahMaCaka <pleahmacaka@gmail.com>
2024-05-27 01:24:26 +0900

    style: pass lint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
-<h1 class="text-3xl font-bold underline text-center">
-	Hello world!
-</h1>
-<button class="btn btn-primary size-full h-full
+<h1 class="text-3xl font-bold underline text-center">Hello world!</h1>
+<button
+	class="btn btn-primary size-full h-full
 							text-white text-3xl"
 >
 	BIG HELLO
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,5 @@ export default {
 	theme: {
 		extend: {}
 	},
-	plugins: [
-		require("daisyui")
-	]
+	plugins: [require("daisyui")]
 }
