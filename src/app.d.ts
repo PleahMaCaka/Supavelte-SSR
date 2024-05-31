e347eba4043c5fa62ef1d21c210f7072af3d3084
SEOA7777 <pleahmacaka@gmail.com>
2024-05-31 13:37:10 +0900

    chore: add generated type in database generic

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,14 @@
 // noinspection JSUnusedGlobalSymbols
 
+import type { Database } from "$lib/ts/gen/supabase"
 import type { Session, SupabaseClient, User } from "@supabase/supabase-js"
 
 declare global {
     namespace App {
         // interface Error {}
-        
+
         interface Locals {
-            supabase: SupabaseClient
+            supabase: SupabaseClient<Database>
             safeGetSession: () => Promise<{ session: Session | null; user: User | null }>
             session: Session | null
             user: User | null
